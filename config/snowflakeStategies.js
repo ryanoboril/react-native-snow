6fed916ae52b0628655bf50e6b1ac7118776e4ae
Paolo Frias <paolo@smartlogic.io>
2018-10-08 17:34:03 -0400

    create file to hold snowflake strategies

---
+++ config/snowflakeStategies.js
@@ -0,0 +1,36 @@
+const lightSnowflakes = [
+  { glyph: '❆', size: 14, offset: '1%', fallDelay: 0, shakeDelay: 0, },
+  { glyph: '❆', size: 24, offset: '5%', fallDelay: 1000, shakeDelay: 1000, },
+  { glyph: '❆', size: 14, offset: '10%', fallDelay: 6000, shakeDelay: 500, },
+  { glyph: '❆', size: 18, offset: '15%', fallDelay: 4000, shakeDelay: 2000, },
+  { glyph: '❆', size: 14, offset: '20%', fallDelay: 2000, shakeDelay: 2000, },
+  { glyph: '❆', size: 24, offset: '25%', fallDelay: 8000, shakeDelay: 3000, },
+  { glyph: '❆', size: 14, offset: '30%', fallDelay: 6000, shakeDelay: 2000, },
+  { glyph: '❆', size: 18, offset: '35%', fallDelay: 2500, shakeDelay: 1000, },
+  { glyph: '❆', size: 14, offset: '40%', fallDelay: 3000, shakeDelay: 1500, },
+];
+
+const mediumSnowflakes = [
+  { glyph: '❆', size: 14, offset: '01%', fallDelay: 500, shakeDelay: 0, },
+  { glyph: '❆', size: 18, offset: '04%', fallDelay: 3900, shakeDelay: 1000, },
+  { glyph: '❆', size: 14, offset: '05%', fallDelay: 7500, shakeDelay: 1000, },
+  { glyph: '❆', size: 24, offset: '07%', fallDelay: 8100, shakeDelay: 500, },
+  { glyph: '❆', size: 14, offset: '10%', fallDelay: 3000, shakeDelay: 2000, },
+  { glyph: '❆', size: 24, offset: '12%', fallDelay: 2200, shakeDelay: 1400, },
+  { glyph: '❆', size: 14, offset: '15%', fallDelay: 6700, shakeDelay: 3000, },
+  { glyph: '❆', size: 24, offset: '17%', fallDelay: 1300, shakeDelay: 2000, },
+  { glyph: '❆', size: 18, offset: '18%', fallDelay: 8800, shakeDelay: 0, },
+  { glyph: '❆', size: 18, offset: '19%', fallDelay: 0, shakeDelay: 500, },
+  { glyph: '❆', size: 14, offset: '19%', fallDelay: 7100, shakeDelay: 2000, },
+  { glyph: '❆', size: 24, offset: '20%', fallDelay: 5900, shakeDelay: 500, },
+  { glyph: '❆', size: 18, offset: '22%', fallDelay: 3400, shakeDelay: 1700, },
+  { glyph: '❆', size: 14, offset: '24%', fallDelay: 1900, shakeDelay: 4000, },
+  { glyph: '❆', size: 18, offset: '27%', fallDelay: 3400, shakeDelay: 900, },
+  { glyph: '❆', size: 14, offset: '28%', fallDelay: 750, shakeDelay: 2500, },
+  { glyph: '❆', size: 18, offset: '30%', fallDelay: 4700, shakeDelay: 3000, },
+];
+
+module.exports = {
+  lightSnowflakes,
+  mediumSnowflakes,
+};
--- src/Snow.js
+++ src/Snow.js
@@ -6,28 +6,10 @@ import {
   Dimensions,
 } from 'react-native';
 import { oneOf, shape, string } from 'prop-types';
+import { lightSnowflakes, mediumSnowflakes } from '../config/snowflakeStrategies';
 import Snowflake from './Snowflake';
 
 const windowHeight = Dimensions.get('window').height + (Dimensions.get('window').height * .1);
-
-const lightSnowflakes = [
-  { size: 14, offset: '1%', fallDelay: 0, shakeDelay: 0, }, { size: 24, offset: '5%', fallDelay: 1000, shakeDelay: 1000, },
-  { size: 14, offset: '10%', fallDelay: 6000, shakeDelay: 500, }, { size: 18, offset: '15%', fallDelay: 4000, shakeDelay: 2000, },
-  { size: 14, offset: '20%', fallDelay: 2000, shakeDelay: 2000, }, { size: 24, offset: '25%', fallDelay: 8000, shakeDelay: 3000, },
-  { size: 14, offset: '30%', fallDelay: 6000, shakeDelay: 2000, }, { size: 18, offset: '35%', fallDelay: 2500, shakeDelay: 1000, },
-  { size: 14, offset: '40%', fallDelay: 3000, shakeDelay: 1500, },
-];
-
-const mediumSnowflakes = [
-  { size: 14, offset: '1%', fallDelay: 0, shakeDelay: 0, }, { size: 24, offset: '5%', fallDelay: 1000, shakeDelay: 1000, },
-  { size: 14, offset: '10%', fallDelay: 6000, shakeDelay: 500, }, { size: 18, offset: '15%', fallDelay: 4000, shakeDelay: 2000, },
-  { size: 14, offset: '20%', fallDelay: 2000, shakeDelay: 2000, }, { size: 24, offset: '25%', fallDelay: 8400, shakeDelay: 3000, },
-  { size: 14, offset: '30%', fallDelay: 6000, shakeDelay: 2000, }, { size: 18, offset: '35%', fallDelay: 2500, shakeDelay: 1000, },
-  { size: 14, offset: '40%', fallDelay: 3000, shakeDelay: 1500, }, { size: 24, offset: '7%', fallDelay: 600, shakeDelay: 500 },
-  { size: 18, offset: '22%', fallDelay: 1700, shakeDelay: 1700 }, { size: 18, offset: '34%', fallDelay: 3350, shakeDelay: 3000 },
-  { size: 24, offset: '13%', fallDelay: 4300, shakeDelay: 4000 }, { size: 16, offset: '22%', fallDelay: 4760, shakeDelay: 1500 },
-  { size: 16, offset: '27%', fallDelay: 6100, shakeDelay: 900 }, { size: 16, offset: '38%', fallDelay: 7800, shakeDelay: 2500 },
-];
 
 export default class Snow extends Component {
   render() {
@@ -37,11 +19,11 @@ export default class Snow extends Component {
       <View style={styles.view} pointerEvents="none">
         {
           snowflakes.map((snowflake, i) => {
-            const { size, offset, fallDelay, shakeDelay } = snowflake;
+            const { glyph, size, offset, fallDelay, shakeDelay } = snowflake;
             return (
               <Snowflake
                 key={`snowflake-${i}`}
-                glyph='❆'
+                glyph={glyph}
                 size={size}
                 offset={offset}
                 fallDelay={fallDelay}
